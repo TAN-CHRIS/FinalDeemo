8fe6585157654c89617e4ae2d3c36d3fa78446b4
qiaqi <2903561172@qq.com>
2018-11-18 00:34:11 +0800

    update

--- app/src/main/java/com/example/chris/finaldeemo/CheckInActivity.java
+++ app/src/main/java/com/example/chris/finaldeemo/CheckInActivity.java
@@ -1,9 +1,13 @@
 package com.example.chris.finaldeemo;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
+
+import org.w3c.dom.Text;
 
 public class CheckInActivity extends AppCompatActivity {
 
@@ -15,7 +19,11 @@ public class CheckInActivity extends AppCompatActivity {
         //沉浸式状态栏
         QMUIStatusBarHelper.translucent(this);
 
-
+        TextView textView = findViewById(R.id.button2);
+        TextView textView2 = findViewById(R.id.textView2);
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"font/HanYiYanKaiW-2.ttf");
+        textView.setTypeface(typeface, typeface.BOLD_ITALIC);
+        textView2.setTypeface(typeface,Typeface.BOLD);
 
     }
 }
